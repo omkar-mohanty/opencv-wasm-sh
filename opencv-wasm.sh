51cbe09950e1675c8238d95f58a67983782a47cf
Omkar Mohanty <franzohouser@gmail.com>
2022-10-07 13:40:15 +0530

    added command to change directory to /lib

--- opencv-wasm.sh
+++ opencv-wasm.sh
@@ -9,6 +9,8 @@ mkdir -p build && cd build
 cmake  -DBUILD_SHARED_LIBS=OFF ../opencv-4.x
 # Build
 cmake --build .
+# change directory to where all .a static files are
+cd lib
 #use wasicc to build .wasm file
 wasicc libopencv_calib3d.a libopencv_flann.a libopencv_imgproc.a libopencv_stitching.a libopencv_core.a libopencv_gapi.a libopencv_ml.a libopencv_ts.a libopencv_dnn.a  libopencv_highgui.a libopencv_objdetect.a libopencv_video.a libopencv_features2d.a libopencv_imgcodecs.a libopencv_photo.a libopencv_videoio.a -o opencv.wasm -Wl,--no-entry
 
